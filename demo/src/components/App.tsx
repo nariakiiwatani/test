07ed7442d8710f447bfa0b0801f0819dfb25cea2
nariakiiwatani <nariakiiwatani@annolab.com>
2020-08-11 22:14:21 +0900

    first import

---
+++ demo/src/components/App.tsx
@@ -0,0 +1,12 @@
+
+import React, { useState } from 'react'
+// This import is only for this demo to use the newest version.
+import { ModuleName } from '../../../'
+// you would do `npm i your-package-name` and import it
+// import { ModuleName } from 'your-package-name'
+
+const App = (props: {}) => {
+	return (<></>)
+}
+
+export default App;
